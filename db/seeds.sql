82b1e18dfea35135343451cc693c8b80f2eb8c7d
Enrique Garduno <enriquegarduno@Macintosh.local>
2016-08-06 18:20:06 -0500

    done

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Cheese Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Bacon Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Chicken Burger', FALSE, now());
+	
