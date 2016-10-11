ba0b2ead8c0ffcd44d071a915457e27d70e6704a
Enrique Garduno <enriquegarduno@Macintosh.local>
2016-10-10 19:33:10 -0500

    first pages commit

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Cheese Burger', FALSE, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Bacon Burger', FALSE, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('McDowell Chicken Burger', FALSE, now());
-	
